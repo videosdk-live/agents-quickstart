fffb961905bef7c6d11de0deb809c94ab4a92d72
poojan <poojan@videosdk.live>
2025-10-15 15:55:29 +0530

    updated agents

--- mobile-quickstarts/flutter/agent-flutter.py
+++ mobile-quickstarts/flutter/agent-flutter.py
@@ -1,63 +1,56 @@
-import asyncio
-import logging
-from videosdk.agents import Agent, AgentSession, RealTimePipeline, function_tool, WorkerJob, JobContext, RoomOptions
+from videosdk.agents import Agent, AgentSession, RealTimePipeline,JobContext, RoomOptions, WorkerJob
 from videosdk.plugins.google import GeminiRealtime, GeminiLiveConfig
+import logging
+logging.getLogger().setLevel(logging.INFO)
 
-logging.getLogger().setLevel(logging.CRITICAL)
-class RealtimeAgent(Agent):
+class MyVoiceAgent(Agent): 
     def __init__(self):
         super().__init__(
-            instructions="""You are a high-energy game-show host guiding the caller to guess a secret number from 1 to 100 to win 1,000,000$.""",
+            instructions="You are a high-energy game-show host guiding the caller to guess a secret number from 1 to 100 to win 1,000,000$.",
         )
 
     async def on_enter(self) -> None:
         await self.session.say("Welcome to the Videosdk's AI Agent game show! I'm your host, and we're about to play for 1,000,000$. Are you ready to play?")
-
+    
     async def on_exit(self) -> None:
         await self.session.say("Goodbye!")
 
-async def entrypoint(ctx: JobContext):
-
-# Initialize Model
+async def start_session(context: JobContext):
+    agent = MyVoiceAgent()
     model = GeminiRealtime(
         model="gemini-2.0-flash-live-001",
+        # When GOOGLE_API_KEY is set in .env - DON'T pass api_key parameter
+        # api_key="AIXXXXXXXXXXXXXXXXXXXX", 
         config=GeminiLiveConfig(
-            voice="Leda",
-            response_modalities=["AUDIO"],
+            voice="Leda", # Puck, Charon, Kore, Fenrir, Aoede, Leda, Orus, and Zephyr.
+            response_modalities=["AUDIO"]
         )
     )
 
     pipeline = RealTimePipeline(model=model)
-    
-    agent = RealtimeAgent()
-    
     session = AgentSession(
         agent=agent,
-        pipeline=pipeline,
+        pipeline=pipeline
     )
 
-    try:
-        await ctx.connect()
-        print("Waiting for participant...")
-        await ctx.room.wait_for_participant()
-        print("Participant joined")
-        await session.start()
-        await asyncio.Event().wait()
-    except KeyboardInterrupt:
-        print("Shutting down...")
-    finally:
-        await session.close()
-        await ctx.shutdown()
+    def on_transcription(data: dict):
+        role = data.get("role")
+        text = data.get("text")
+        print(f"[TRANSCRIPT][{role}]: {text}")
+    pipeline.on("realtime_model_transcription", on_transcription)
+
+    await context.run_until_shutdown(session=session,wait_for_participant=True)
 
 def make_context() -> JobContext:
-    # Static meeting ID - same as used in frontend
-    room_options = RoomOptions(room_id="YOUR_MEETING_ID", name="Sandbox Agent", playground=True) 
-    return JobContext(
-        room_options=room_options
-        )
+    room_options = RoomOptions(
+        # Static meeting ID - same as used in frontend
+        room_id="YOUR_MEETING_ID", # Replace it with your actual room_id
+        name="Gemini Agent",
+        playground=True,
+    )
 
+    return JobContext(room_options=room_options)
 
 if __name__ == "__main__":
-
-    job = WorkerJob(entrypoint=entrypoint, jobctx=make_context)
+    job = WorkerJob(entrypoint=start_session, jobctx=make_context)
     job.start()
